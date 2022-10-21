84a8c82b2df6340e4c5c458579033970639772a2
Vikas Shenoy <vikashenoy26@gmail.com>
2022-10-22 09:44:24 +1300

    Add data models

---
+++ models/Exercise.ts
@@ -0,0 +1,11 @@
+import { ExerciseLog } from "./ExerciseLog"
+
+export interface Exercise {
+  uuid: string
+  exerciseGroupUuid: string
+  name: string
+  description?: string
+  initialBpm?: number
+  targetBpm?: number
+  logs?: ExerciseLog[]
+}
